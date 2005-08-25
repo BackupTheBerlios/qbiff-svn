1f749c431a80271c9337bf62ec9dcc98f01b6a0c
ms <ms@7cb81ca6-2be6-0310-892c-d2e825cd7cb7>
2005-08-25 09:32:57 +0000

    - fixed path

--- qbiff-head/config.h
+++ qbiff-head/config.h
@@ -2,7 +2,7 @@
 // root of the maildir tree
 //---------------------------------------
 #define MY_FOLDER      "/suse/ms/Mail/"
-#define MY_MAILCLIENT  "/suse/ms/bin/qbiff/ssl/readmail"
+#define MY_MAILCLIENT  "/usr/share/qbiff/readmail"
 
 //=======================================
 // network stuff
@@ -14,18 +14,18 @@
 //=======================================
 // certification stuff
 //---------------------------------------
-#define CAFILE   "cert-server/rootcert.pem"
-#define DH1024   "cert-server/dh1024.pem"
-#define DH512    "cert-server/dh512.pem"
+#define CAFILE   "/usr/share/qbiff/cert-server/rootcert.pem"
+#define DH1024   "/usr/share/qbiff/cert-server/dh1024.pem"
+#define DH512    "/usr/share/qbiff/cert-server/dh512.pem"
 #define CADIR    NULL
 
 //=======================================
 // server/client certs
 //---------------------------------------
-#define SERVER_CERTFILE  "cert-server/server.pem"
-#define CLIENT_CERTFILE  "cert-client/client.pem"
+#define SERVER_CERTFILE  "/usr/share/qbiff/cert-server/server.pem"
+#define CLIENT_CERTFILE  "/usr/share/qbiff/cert-client/client.pem"
 
 //=======================================
 // pixmaps
 //---------------------------------------
-#define PIXINFO  "/suse/ms/bin/qbiff/ssl/pixmaps/tooltip.xpm"
+#define PIXINFO  "/usr/share/qbiff/pixmaps/tooltip.xpm"
